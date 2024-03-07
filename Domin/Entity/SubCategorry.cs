cd6fbd5c21766e146b8f38674211adb4d6a8da32
Ali Samy <ali.samy@miclife.com>
2024-03-07 15:26:33 +0200

    first

---
+++ Domin/Entity/SubCategorry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domin.Entity
+{
+    public class SubCategorry
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Resources.ResourceData), ErrorMessageResourceName = ("SubCategoryName"))]
+        [MaxLength(20, ErrorMessageResourceType = typeof(Resources.ResourceData), ErrorMessageResourceName = ("MaxLength"))]
+        [MinLength(3, ErrorMessageResourceType = typeof(Resources.ResourceData), ErrorMessageResourceName = ("MinLength"))]
+        public Guid Name { get; set; }
+        public Guid CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+        public int CurrentStaut { get; set; }
+    }
+}
---
+++ Infrastructure/Data/FreeBookDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class FreeBookDbContext : IdentityDbContext
+    {
+        public FreeBookDbContext(DbContextOptions<FreeBookDbContext> options ) : base( options ) 
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           
+
+            builder.Entity<IdentityUser>().ToTable("Users", "Identity");
+            builder.Entity<IdentityRole>().ToTable("Roles", "Identity");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Identity");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim", "Identity");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin", "Identity");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim", "Identity");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken", "Identity");
+        }
+    }
+}
